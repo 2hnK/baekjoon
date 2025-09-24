62e55c36ac3f1f8c521cc7f335ee2ef0f6515095
2hnk <156529176+2hnK@users.noreply.github.com>
2025-09-25 01:24:45 +0900

    Title: 함수를 이용한 소수 판별, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 250925/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 250925/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -6,6 +6,7 @@ public class Main {
         int cnt = 0;
 
         for (int i = a; i <= b; i++) {
+            if(i==2) cnt += 2;
             for (int j = 2; j < i; j++) {
                 if((i % j) == 0)
                     break;
