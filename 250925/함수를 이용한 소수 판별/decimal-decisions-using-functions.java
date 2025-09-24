21dcd1613c67b1f2a5bcc91dcc26b4727ee770c7
2hnk <156529176+2hnK@users.noreply.github.com>
2025-09-25 01:12:12 +0900

    Title: 함수를 이용한 소수 판별, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250925/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int counting(int a, int b) {
+        int cnt = 0;
+
+        for (int i = a; i <= b; i++) {
+            for (int j = 2; j < i; j++) {
+                if((i % j) == 0)
+                    break;
+                else if(j == i-1)
+                    cnt += i;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Please write your code here.
+
+        int res = counting(a, b);
+        System.out.println(res);
+    }
+}
