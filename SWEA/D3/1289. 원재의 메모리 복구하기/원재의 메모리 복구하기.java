2a396f0d0187a2439c73c2ff8800ce200835b20b
2hnk <156529176+2hnK@users.noreply.github.com>
2026-01-28 15:41:59 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 78 ms, Memory: 24,832 KB -BaekjoonHub

---
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int TC = 1; TC <= T; TC++) {
+            sb.append("#" + TC + " ");
+            char[] memory = br.readLine().toCharArray();
+            int cnt = 0;
+
+            // 시작값이 1이면 전환
+            if (memory[0] == '1') {
+                cnt++;
+            }
+
+            // 이전값과 다르면 전환
+            for (int i = 1; i < memory.length; i++) {
+                if (memory[i - 1] == memory[i]) {
+                    continue;
+                } else {
+                    cnt++;
+                }
+            }
+
+            sb.append(cnt + "\n");
+        }
+
+        System.out.print(sb);
+    }
+}
