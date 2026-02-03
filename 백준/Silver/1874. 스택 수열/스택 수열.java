9aa9c7ff064eaac2f52b0b06a89340a8061cac3d
2hnk <156529176+2hnK@users.noreply.github.com>
2026-02-03 15:51:54 +0900

    [Silver II] Title: 스택 수열, Time: 248 ms, Memory: 26972 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 class Main {
     public static void main(String[] args) throws Exception {
@@ -16,7 +17,7 @@ class Main {
         }
 
         int ptr = 0;
-        Stack<Integer> s = new Stack<>();
+        Deque<Integer> s = new ArrayDeque<>();
         for (int i = 0; i < N; i++) {
             s.push(i + 1);
             sb.append("+").append("\n");
