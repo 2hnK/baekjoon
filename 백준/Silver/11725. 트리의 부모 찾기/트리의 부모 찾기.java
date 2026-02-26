572f1f802a4fdfdbe7ef07a11bd6127acdf1c237
2hnk <156529176+2hnK@users.noreply.github.com>
2026-02-27 01:08:37 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 560 ms, Memory: 57376 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] parent;
+    static boolean[] visited;
+    static ArrayList<Integer>[] adjList;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        parent = new int[N + 1];
+        visited = new boolean[N + 1];
+        adjList = new ArrayList[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            adjList[x].add(y);
+            adjList[y].add(x);
+        }
+
+        bfs(1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= N; i++) {
+            sb.append(parent[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            for (int next : adjList[cur]) {
+                if (!visited[next]) {
+                    parent[next] = cur;
+                    visited[next] = true;
+                    q.offer(next);
+                }
+            }
+        }
+    }
+}
